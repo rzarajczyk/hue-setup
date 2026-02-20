a64bfbfa856d130187afcf7e3c91a687c9c9187e
rzarajczyk <rzarajczyk@gmail.com>
2026-02-20 22:20:44 +0100

    Hue button added

--- src/main/kotlin/pl/zarajczyk/huesetup/managers/RemoteHelper.kt
+++ src/main/kotlin/pl/zarajczyk/huesetup/managers/RemoteHelper.kt
@@ -10,11 +10,12 @@ sealed interface RemoteHelper {
         fun get(device: Device) = when (device.product_data.model_id) {
             "RWL021" -> HueDimmerSwitchV1
             "RWL022" -> HueDimmerSwitchV2
-            "ROM001" -> HueSmartButton
+            "ROM001" -> HueSmartButtonV1
+            "RDM005" -> HueSmartButtonV2
             else -> throw RuntimeException("Unsupported remote controller: ≪${device.product_data.model_id}≫")
         }
 
-        fun listSupportedModelIds() = listOf("RWL021", "RWL022", "ROM001")
+        fun listSupportedModelIds() = listOf("RWL021", "RWL022", "ROM001", "RDM005")
     }
 
 }
@@ -58,7 +59,7 @@ private data object HueDimmerSwitchV1 : RemoteHelper {
     }
 }
 
-private data object HueSmartButton : RemoteHelper {
+private data object HueSmartButtonV1 : RemoteHelper {
     override fun getButtonCode(button: String, pressType: String): Int {
         val base = 1000
         return when (pressType) {
@@ -74,6 +75,24 @@ private data object HueSmartButton : RemoteHelper {
     override fun getRuleName(sensorId: String, button: String, pressType: String) = "Hue smart button $sensorId"
 
 }
+
+private data object HueSmartButtonV2 : RemoteHelper {
+    override fun getButtonCode(button: String, pressType: String): Int {
+        val base = 1000
+        return when (pressType) {
+            "initial_press" -> base
+            "repeat" -> base + 1
+            "short_release" -> base + 2
+            "long_release" -> base + 3
+            "long_press" -> base + 10
+            else -> throw RuntimeException("Unsupported press type: ≪$pressType≫ for ${this.javaClass.simpleName}")
+        }
+    }
+
+    override fun getRuleName(sensorId: String, button: String, pressType: String) = "Hue smart button $sensorId"
+
+}
+
 
 private data object HueDimmerSwitchV2 : RemoteHelper {
     override fun getButtonCode(button: String, pressType: String): Int {
